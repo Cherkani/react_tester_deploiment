122a6a2bf6509aa8cb3e4e8770e1e0210c649445
Aymen Cherkani <cherkaniaymen1@gmail.com>
2023-12-31 16:32:36 +0100

    login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,61 +3,60 @@ import { useState } from "react"
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 const Login = () => {
   const depurl = import.meta.env.VITE_DEP_URL;
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [user, setUser] = useState({
     email:"",
     password:""
-  })
+  });
   const notify = (mssg) => {
     toast.success(`${mssg}`, {
-        position: 'bottom-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'colored',
+      position: 'bottom-right',
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
     });
-};
-  const [err,setErr] = useState("")
+  };
+  const [err, setErr] = useState("");
 
-  const handleInput = (e)=>{
-    setUser({...user,[e.target.name]:e.target.value})
-
-  }
+  const handleInput = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
-  const submit = async()=>{
-    console.log(user)
-   
+
+  const submit = async () => {
+    console.log(user);
+
     try {
-      const res = await axios.post(`${depurl}/api/users/login`,user)
-      console.log(res.data)
+      const res = await axios.post(`${depurl}/api/users/login`, user);
+      console.log(res.data);
       setUser({
         email:"",
         password:""
-      })
-      localStorage.setItem("userlogin",JSON.stringify(res.data))
+      });
+      localStorage.setItem("userlogin", JSON.stringify(res.data));
       setTimeout(() => {
-        notify(res.data.message)
+        notify(res.data.message);
       }, 1000);
-     
-    // console.log(JSON.parse(localStorage.getItem("userlogin")).user.role)
-    const role = res.data.role
-    if(role == 'professor'){
-      window.location = role+'/profil'
-    }
-    else{
-      window.location = role
-    }
-    
+
+      const role = res.data.role;
+      if (role === 'professor') {
+        window.location = role + '/profil';
+      } else {
+        window.location = role;
+      }
+
     } catch (error) {
-      console.log(error.response.data.message)
+      console.log(error.response.data.message);
       toast.error(`${error.response.data.message}`, {
         position: 'bottom-right',
         autoClose: 2000,
@@ -67,115 +66,99 @@ const Login = () => {
         draggable: true,
         progress: undefined,
         theme: 'colored',
-    });
-      
+      });
     }
-    
-  }
+  };
 
   return (
-<div className={`${isDarkMode ? 'dark' : ''}`}>
-    <div className={`dark:bg-slate-900 bg-gray-100 flex h-full items-center py-16 min-h-screen font-[Poppins]`}>
-    <main class="w-full max-w-md mx-auto p-6">
-      <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <div class="p-4 sm:p-7">
-          <div class="text-center">
-            <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
-            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
-             
+    <div style={{ background: 'beige' }}>
+   <div className={`dark:bg-slate-900 bg-blue-300 flex h-full items-center py-16 min-h-screen font-[Poppins]`}>
+ <main className="w-full max-w-md mx-auto p-6">
+       <div className="mt-7 bg-tooth border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-4 sm:p-7" style={{ background: '#ffffff' }}>
+              <div className="text-center">
+                <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Login</h1>
             
-            </p>
-          </div>
-
-          <div class="mt-5">
-            
-
-            <div class="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600"></div>
-
-            
-            
-              <div class="grid gap-y-4">
-                
-                <div>
-                  <label for="email" class="block text-sm mb-2 dark:text-white">Email address</label>
-                  <div class="relative">
-                    <input type="email" id="email" name="email" value={user.email} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error"/>
-                    <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
-                      <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                      </svg>
-                    </div>
-                  </div>
-                  <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
-                </div>
-           
-                <div>
-                  <div class="flex justify-between items-center">
-                    <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
-                       </div>
-                  <div class="relative">
-                    <input type="password" id="password" name="password" value={user.password} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="password-error"/>
-                    <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
-                      <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                      </svg>
-                    </div>
-                  </div>
-                  <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
-                </div>
-                
-
-                
-                {err && <p class="text-sm text-red-600 mt-2" >{err}</p>}
-                
-                <div className="flex gap-4">
-  <a
-    href="/register"
-    className="flex-1 py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-gray-300 text-black hover:bg-gray-400 hover:text-black disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-gray-600"
-  >
-    Sign up
-  </a>
-  <button
-    onClick={submit}
-    className="flex-1 py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-black text-white hover:bg-black hover:text-white disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-black"
-  >
-    Sign in
-  </button>
-</div>
-
-
               </div>
-            
-            
+
+              <div className="mt-5">
+         
+                <div className="grid gap-y-4">
+                  <div className="relative">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={user.email}
+                      onChange={handleInput}
+                      className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                      placeholder="Email address"
+                      required
+                      aria-describedby="email-error"
+                    />
+                    <div className="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                      <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  <div className="relative">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={user.password}
+                      onChange={handleInput}
+                      className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                      placeholder="Password"
+                      required
+                      aria-describedby="password-error"
+                    />
+                    <div className="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                      <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  {err && <p className="text-sm text-red-600 mt-2">{err}</p>}
+
+                  <div className="flex gap-4">
+                    <a
+                      href="/register"
+                      className="flex-1 py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-gray-300 text-black hover:bg-gray-400 hover:text-black disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-gray-600"
+                    >
+                     register
+                    </a>
+                    <button
+                      onClick={submit}
+                      className="flex-1 py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-black text-white hover:bg-black hover:text-white disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-black"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+         
+        </main>
       </div>
-      <div className="absolute top-4 right-4">
-          <button
-            onClick={toggleDarkMode}
-            className="bg-gray-200 dark:bg-gray-700 rounded-full p-2 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            {isDarkMode ? '🌞' : '🌙'}
-          </button>
-        </div>
-    </main>
-  </div>
-  <ToastContainer
-                position="bottom-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-  </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+};
 
-
-  )
-}
-
-export default Login
+export default Login;
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -76,12 +76,12 @@ const Register = () => {
 
   return (
     <div className={`${isDarkMode ? 'dark' : ''}`}>
-      <div className='dark:bg-slate-900 bg-gray-100 flex h-full items-center py-10 min-h-screen font-[Poppins]'>
-        <main class="w-full max-w-lg  mx-auto p-6">
+     <div className={`dark:bg-slate-900 bg-blue-300 flex h-full items-center py-16 min-h-screen font-[Poppins]`}>
+   <main class="w-full max-w-lg  mx-auto p-6">
           <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <div class="p-4 sm:p-7">
               <div class="text-center">
-                <h1 class="block  text-2xl font-bold text-gray-800 dark:text-white">Sign up</h1>
+                <h1 class="block  text-2xl font-bold text-gray-800 dark:text-white">Register</h1>
                 <p class="mt-2   text-sm text-gray-600 dark:text-gray-400">
                 
                 
@@ -92,11 +92,21 @@ const Register = () => {
               <div class="mt-5">
 
                 <div class="grid gap-y-4">
-
-                  <div>
-                    <label for="nom" class="block text-sm mb-2 dark:text-white">First Name </label>
+                <div>
                     <div class="relative">
-                      <input type="text" id="nom" name="firstName" value={user.firstName} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
+                      <input  name="grade"  placeholder="grade" value={user.grade} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
+                      <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                        <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                  </div>
+                  <div>
+                   
+                    <div class="relative">
+                      <input type="text"  placeholder="firstName" id="nom" name="firstName" value={user.firstName} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
                       <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                         <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
@@ -106,9 +116,9 @@ const Register = () => {
 
                   </div>
                   <div>
-                    <label for="prenom" class="block text-sm mb-2 dark:text-white">Last Name</label>
+                  
                     <div class="relative">
-                      <input type="text" id="prenom" name="lastName" value={user.lastName} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
+                      <input type="text" id="prenom"  placeholder="lastName" name="lastName" value={user.lastName} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
                       <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                         <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
@@ -118,9 +128,8 @@ const Register = () => {
 
                   </div>
                   <div>
-                    <label for="prenom" class="block text-sm mb-2 dark:text-white">UserName</label>
                     <div class="relative">
-                      <input type="text" id="prenom" name="userName" value={user.userName} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
+                      <input type="text" id="prenom" placeholder="userName" name="userName" value={user.userName} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
                       <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                         <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
@@ -132,9 +141,8 @@ const Register = () => {
                   
                   
                   <div>
-                    <label for="email" class="block text-sm mb-2 dark:text-white">Email address</label>
                     <div class="relative">
-                      <input type="email" id="email" name="email" value={user.email} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
+                      <input type="email" id="email"  placeholder="email" name="email" value={user.email} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
                       <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                         <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
@@ -146,10 +154,10 @@ const Register = () => {
 
                   <div>
                     <div class="flex justify-between items-center">
-                      <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
+                      
                     </div>
                     <div class="relative">
-                      <input type="password" id="password" name="password" value={user.password} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="password-error" />
+                      <input type="password" id="password"  placeholder="password" name="password" value={user.password} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="password-error" />
                       <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                         <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
@@ -158,18 +166,7 @@ const Register = () => {
                     </div>
                     
                   </div>
-                  <div>
-                    <label for="email" class="block text-sm mb-2 dark:text-white">Grade </label>
-                    <div class="relative">
-                      <input  name="grade" value={user.grade} onChange={handleInput} class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
-                      <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
-                        <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                        </svg>
-                      </div>
-                    </div>
-                    <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
-                  </div>
+                 
                   {/* {err && <p class="text-sm text-red-600 mt-2" >{err}</p>} */}
                
               
@@ -178,12 +175,12 @@ const Register = () => {
 <div className="flex gap-4">
 
   <button onClick={submit} className="flex-1 py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-black text-white hover:bg-black hover:text-white disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-black"
->Sign up </button>
+>Register </button>
 <a
     href="/login"
     className="flex-1 py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-gray-300 text-black hover:bg-gray-400 hover:text-black disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-gray-600"
   >
-    Sign in 
+   Login
   </a>
 </div>
                 </div>
